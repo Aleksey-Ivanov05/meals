dd04722e54cfd74baae60fa60f2ae0efdd10d5c5
Aleksey Ivanov <lepri4dw@gmail.com>
2022-12-12 12:54:58 +0600

    Почистил проект, установил нужные библиотеки

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./containers/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="navbar navbar-expand-sm navbar-dark bg-primary">
+        <div className="container-fluid">
+          <span className="navbar-brand">Calorie tracker</span>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={(
+          <Home/>
+        )}/>
+      </Routes>
+    </>
   );
 }
 
